acf91e3b264bc1fef2be352d1580bd0302a7f137
Alejandro Padron <alexpadronn97@gmail.com>
2016-08-22 17:14:19 -0400

    add article url

--- apadron/apadron/urls.py
+++ apadron/apadron/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^articles/', include('articles.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
 ]
