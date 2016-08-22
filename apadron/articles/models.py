7ebd782ca1db727ef445f10d35671e3f25ea754c
Alejandro Padron <alexpadronn97@gmail.com>
2016-08-22 17:35:43 -0400

    add output to article page

--- apadron/articles/models.py
+++ apadron/articles/models.py
@@ -4,3 +4,6 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     body = models.TextField()
+
+    def __str_(self):
+        return self.title
